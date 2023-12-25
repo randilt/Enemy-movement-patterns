e65861669d25da224089084b6d052291e76b5074
RandilT <randiltharusha72@gmail.com>
2023-12-25 17:19:08 +0530

    Import enemy assets

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+
+enemy1 = {
+  x: 0,
+  y: 0,
+  width: 200,
+  height: 200,
+};
