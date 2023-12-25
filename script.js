173384d7bf373a7e086a364f8e6e863ddf78ad00
RandilT <randiltharusha72@gmail.com>
2023-12-25 18:15:35 +0530

    Add randomize speeds and directions for each enemy

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numOfEnemies = 100;
+const enemies = [];
 
 class Enemy {
   constructor() {
@@ -11,22 +13,28 @@ class Enemy {
     this.y = Math.random() * CANVAS_HEIGHT;
     this.width = 100;
     this.height = 100;
+    this.speed = Math.random() * 4 - 2;
   }
   update() {
-    this.x++;
-    this.y++;
+    this.x += this.speed;
+    this.y += this.speed;
   }
   draw() {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
-const enemy1 = new Enemy(10, 80, 100, 100, "red", 1);
+for (let i = 0; i < numOfEnemies; i++) {
+  enemies.push(new Enemy());
+}
+console.log(enemies);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.update();
-  enemy1.draw();
+  enemies.forEach((enemy) => {
+    enemy.draw();
+    enemy.update();
+  });
   requestAnimationFrame(animate);
 }
 animate();
