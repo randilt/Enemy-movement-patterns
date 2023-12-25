e0b82809f1184cbbe88d857e615eb0beefbcc4c1
RandilT <randiltharusha72@gmail.com>
2023-12-25 17:24:23 +0530

    Create enemy class

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
-enemy1 = {
-  x: 0,
-  y: 0,
-  width: 200,
-  height: 200,
-};
+class Enemy {
+  constructor(x, y, width, height, color, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color;
+    this.speed = speed;
+  }
+}
+
+const enemy1 = new Enemy(10, 80, 100, 100, "red", 1);
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemy1.x += 1;
+  ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+  requestAnimationFrame(animate);
+}
+animate();
