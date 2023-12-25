9a93af4661b356c2922262e642f510cd002afab5
RandilT <randiltharusha72@gmail.com>
2023-12-25 18:26:10 +0530

    Animate each enemy

--- script.js
+++ script.js
@@ -7,27 +7,44 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numOfEnemies = 100;
 const enemies = [];
 
+const enemyImage = new Image();
+enemyImage.src = "enemy1.png";
 class Enemy {
   constructor() {
     this.x = Math.random() * CANVAS_WIDTH;
     this.y = Math.random() * CANVAS_HEIGHT;
-    this.width = 100;
-    this.height = 100;
     this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0;
   }
   update() {
     this.x += this.speed;
     this.y += this.speed;
+    //animate sprites
+    this.frame > 4 ? (this.frame = 0) : this.frame++;
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      enemyImage,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
 for (let i = 0; i < numOfEnemies; i++) {
   enemies.push(new Enemy());
 }
-console.log(enemies);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
