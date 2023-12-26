a04dedcb6f3a2ce315d0b2e2e8d3e5f0e3b214e1
RandilT <randiltharusha72@gmail.com>
2023-12-26 13:42:35 +0530

    Enemy 4 complete

--- enemyFour.js
+++ enemyFour.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numOfEnemies = 50;
+const numOfEnemies = 10;
 const enemies = [];
 
 let gameFrame = 0;
@@ -13,26 +13,27 @@ class Enemy {
     this.image = new Image();
     this.image.src = "enemy4.png";
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 218;
-    this.spriteHeight = 177;
+    this.spriteWidth = 213;
+    this.spriteHeight = 213;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
-    this.angle = Math.random() * 500; // Math.random() * 2;
-    this.angleSpeed = Math.random() * 0.5 + 0.5;
-    // this.curve = Math.random() * 200 + 50;
+    this.newX = Math.random() * (canvas.width - this.width);
+    this.newY = Math.random() * (canvas.height - this.height);
+    this.interval = Math.floor(Math.random() * 200 + 50);
   }
   update() {
-    this.x =
-      (canvas.width / 2) * Math.cos(this.angle * (Math.PI / 200)) +
-      (canvas.width / 2 - this.width / 2);
-    this.y =
-      (canvas.height / 2) * Math.sin(this.angle * (Math.PI / 300)) +
-      (canvas.height / 2 - this.height / 2);
-    this.angle += this.angleSpeed;
+    if (gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (canvas.width - this.width);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx / 70;
+    this.y -= dy / 70;
     if (this.x + this.width < 0) {
       this.x = canvas.width;
     }
