9207b8faaa9dd21ca77e38283983460499c8f3e6
RandilT <randiltharusha72@gmail.com>
2023-12-26 10:50:20 +0530

    Randomize flapping speed

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numOfEnemies = 100;
 const enemies = [];
 
-const enemyImage = new Image();
-enemyImage.src = "enemy1.png";
 let gameFrame = 0;
 class Enemy {
   constructor() {
+    this.image = new Image();
+    this.image.src = "enemy1.png";
     this.x = Math.random() * CANVAS_WIDTH;
     this.y = Math.random() * CANVAS_HEIGHT;
     this.speed = Math.random() * 4 - 2;
@@ -20,19 +20,19 @@ class Enemy {
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
     this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
     this.x += this.speed;
     this.y += this.speed;
     //animate sprites
-    if (gameFrame % 2 === 0) {
+    if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
-      enemyImage,
+      this.image,
       this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
