26f8ed38499457167a5dc14ae1a3f01a74bb5df6
RandilT <randiltharusha72@gmail.com>
2023-12-26 11:04:02 +0530

    Make the enemy animation randomized

--- enemy-1.js
+++ enemy-1.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numOfEnemies = 100;
+const numOfEnemies = 10;
 const enemies = [];
 
 let gameFrame = 0;
@@ -12,8 +12,6 @@ class Enemy {
   constructor() {
     this.image = new Image();
     this.image.src = "enemy1.png";
-    this.x = Math.random() * CANVAS_WIDTH;
-    this.y = Math.random() * CANVAS_HEIGHT;
     //this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
@@ -21,6 +19,8 @@ class Enemy {
     this.height = this.spriteHeight / 2.5;
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
   }
   update() {
     this.x += Math.random() * 5 - 2.5;
